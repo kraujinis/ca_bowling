652a830fa1131a64ad7dc68612036ed3d47ece60
kraujinis <kraujinis@gmail.com>
2023-03-03 23:03:20 +0200

    ai

--- bowling/calculation.py
+++ bowling/calculation.py
@@ -1,25 +1,20 @@
 import random
 
-frame_score = []
 
-throw = random.randint(0, 10)
-
-
-if throw == 10:
-    print('STRIKE')
-    frame_score.append(throw)
-elif throw != 10:
-    frame_score.append(throw)
-    print(f'Points for first throw: {throw}')
-    if throw < 10:
-        y = random.randint(0, 10 - throw)
-        print(f'Points for second throw: {y}')
-        frame_score.append(y)
-    stride = throw + y
-    if stride == 10:
-        print('STRIDE')
-        if stride == 0:
-            print('!! 0 !!')
+def generate_throw_points():
+    return random.randint(0, 10)
 
 
-print(frame_score)
+    
+           
+# def calculate_stride(): 
+#     if throw < 10:
+#         y = random.randint(0, 10 - throw)
+#         print(f'Points for second throw: {y}')
+#         frame_score.append(y)
+#     stride = throw + y
+#     if stride == 10:
+#         print('STRIDE')
+#     if stride == 0:
+#         print('!! 0 !!')
+
--- bowling/main.py
+++ bowling/main.py
@@ -1,4 +1,8 @@
+
 from getkey import getkey, keys
+import calculation as calc
+import random
+import time
 
 
 class Player:
@@ -8,8 +12,39 @@ class Player:
 
 
 
+    
 
-key = getkey()
-if key == keys.UP:
-    print('keys UP')
+
+throw_points = []
+
+
+
+while True:
+    
+    
+    key = getkey()
+    rnd_number = random.randint(0, 10)
+    
+    if key == keys.UP:
+        if rnd_number == 10:
+            throw_points.append(rnd_number)
+            print('STRIKE')
+            break
+        while True:
+            if key == keys.UP:
+                if rnd_number != 10:
+                    throw_points.append(rnd_number)
+                    print(f'first number: {rnd_number}')
+                    time.sleep(1)
+                    key == 0
+                if key == keys.UP:
+                    a = random.randint(0, 10 - rnd_number)
+                    throw_points.append(a)
+                    print(f'second number: {a}')
+                    if (rnd_number + a) == 10:
+                        print('STRIDE')
+                    break
+
+
+print(throw_points)
 
