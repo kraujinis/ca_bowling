757d350c3d9fe3baf7706b24afb41152e5b1f174
kraujinis <kraujinis@gmail.com>
2023-03-04 22:43:13 +0200

    done throw calculation

--- bowling/calculation.py
+++ bowling/calculation.py
@@ -1,20 +1,42 @@
 import random
+import keyboard
+import logging
+
+logging.basicConfig(level=logging.DEBUG, filename='data.log', filemode='a',
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    datefmt='%d/%m/%Y %H:%M:%S')
 
 
-def generate_throw_points():
-    return random.randint(0, 10)
+def throw_calculation() -> list:
+    throw_points = []
+    while True:
+        
+        rnd_number = random.randint(0, 10)  # start generate random number
+        keyboard.wait('space')  # wait 'space' push
+        
+        if rnd_number == 10:
+            throw_points.append(rnd_number)
+            logging.info(f"Catched STRIKE !! {rnd_number}")
+            print('STRIKE !')
+            break
+        
+        keyboard.wait('space')
+        throw_points.append(rnd_number) # append first result of throw
+        logging.info(f"Catched first throw if not STRIKE !! {rnd_number}")
+        
+        keyboard.wait('space')
+        second_random = random.randint(0, 10 - rnd_number)
+        throw_points.append(second_random)  # append second result of throw
+        logging.info(f"Catched second throw if not STRIKE !! {second_random}")
+        stride = rnd_number + second_random
+        
+        if stride == 10:
+            print('STRIDE !')
+            logging.info(f"Catched STRIDE !! {stride}")
+            break
+        break
+    return throw_points
 
 
-    
-           
-# def calculate_stride(): 
-#     if throw < 10:
-#         y = random.randint(0, 10 - throw)
-#         print(f'Points for second throw: {y}')
-#         frame_score.append(y)
-#     stride = throw + y
-#     if stride == 10:
-#         print('STRIDE')
-#     if stride == 0:
-#         print('!! 0 !!')
-
+if __name__ == "__main__":
+    throw_calculation()
--- bowling/main.py
+++ bowling/main.py
@@ -1,5 +1,6 @@
 
-import keyboard
+from calculation import throw_calculation
+
 
 class Player:
     def __init__(self, player_name: str, score: int) -> None:
@@ -7,44 +8,4 @@ class Player:
         self.score = score
 
 
-
-    
-
-
-throw_points = []
-
-
-
-# while True:
-    
-    
-#     key = getkey()
-#     rnd_number = random.randint(0, 10)
-    
-#     if key == keys.UP:
-#         if rnd_number == 10:
-#             throw_points.append(rnd_number)
-#             print('STRIKE')
-#             break
-#         while True:
-#             if key == keys.UP:
-#                 if rnd_number != 10:
-#                     throw_points.append(rnd_number)
-#                     print(f'first number: {rnd_number}')
-#                     time.sleep(1)
-#                     key == 0
-#                 if key == keys.UP:
-#                     a = random.randint(0, 10 - rnd_number)
-#                     throw_points.append(a)
-#                     print(f'second number: {a}')
-#                     if (rnd_number + a) == 10:
-#                         print('STRIDE')
-#                     break
-
-
-# print(throw_points)
-
-while True:
-    keyboard.wait('space')
-    print('space was pressed! Waiting on it again...')
-    break
+print(throw_calculation())
