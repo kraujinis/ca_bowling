a2841131f8bf427803e56fedfcaa2ff9f9b38ac2
kraujinis <kraujinis@gmail.com>
2023-03-07 21:56:10 +0200

    oh

--- bowling/calculation.py
+++ bowling/calculation.py
@@ -14,7 +14,7 @@ def one_throw() -> int:
     return rnd_number
 
 
-def throws() -> list:
+def throws() -> list:  # Metimai
 
     throw_points = []
 
@@ -22,7 +22,7 @@ def throws() -> list:
 
         keyboard.wait('space')  # wait 'space' push
         rnd_number = random.randint(0, 10)
-        print(f'1-as metimas. Gauti taškai: {rnd_number}')
+        print(f'Gauti taškai: {rnd_number}')
 
         if rnd_number == 10:
             print('^ ^ STRIKE ^ ^')
@@ -34,7 +34,7 @@ def throws() -> list:
             logging.info(f"Catched first throw if not STRIKE ! {rnd_number}")
             keyboard.wait('space')
             second_random = random.randint(0, 10 - rnd_number)
-            print(f'2-as metimas. Gauti taškai: {second_random}')
+            print(f'Gauti taškai: {second_random}')
             throw_points.append(second_random)
             logging.info(f"Catched second throw if not STRIKE ! {second_random}")
             spare = rnd_number + second_random
@@ -45,43 +45,127 @@ def throws() -> list:
             return throw_points
 
 
-def number_of_throws() -> None:
+# def number_of_throws() -> None:
+#     i = 0
+#     result = {}
+#     while i < 10:
+#         print(f"> {i + 1} < BANDYMAS")
+#         i += 1
+#         value_of_throw = throws()
+#         result[i] = value_of_throw
+
+#         if i == 10:
+#             if result[i][0] == 10:
+#                 last_throw = []
+#                 last_throw.append(result[i][0])
+
+#                 for x in range(2):
+#                     x += 2
+#                     a = one_throw()
+#                     print(f'{x}-as metimas. Gauti taškai: {a}')
+#                     last_throw.append(a)
+
+#                 result[i] = last_throw
+#                 break
+
+#             elif sum(result[i]) == 10:
+#                 a = value_of_throw
+#                 b = one_throw()
+#                 print(f'3-as metimas. Gauti taškai: {b}')
+#                 a.append(b)
+#                 result[i] = a
+#                 break
+
+#     print(result)  # DEL
+
+
+def number_of_throws() -> dict:  # Užėjimai ir metimai, metimo vertės užrašymas
     i = 0
     result = {}
     while i < 10:
         print(f"> {i + 1} < BANDYMAS")
         i += 1
-        value_of_throw = throws()
-        result[i] = value_of_throw
+
+        result[str(i)] = throws()
 
         if i == 10:
-            if result[i][0] == 10:
+            if result[str(i)][0] == 10:  # STRIKE
                 last_throw = []
-                last_throw.append(result[i][0])
-
-                for x in range(2):
-                    x += 2
+                last_throw.append(result[str(i)][0])
+                for n in range(2, 4):
                     a = one_throw()
-                    print(f'{x}-as metimas. Gauti taškai: {a}')
+                    print(f'Gauti taškai: {a}')
                     last_throw.append(a)
-
-                result[i] = last_throw
+                
+                result[str(i)] = last_throw
                 break
 
-            elif sum(result[i]) == 10:
-                a = value_of_throw
+            elif sum(result[str(i)]) == 10:  # SPARE
+                spare_list = []
+
+                for n in result[str(i)]:
+                    spare_list.append(n)
+
                 b = one_throw()
-                print(f'3-as metimas. Gauti taškai: {b}')
-                a.append(b)
-                result[i] = a
+                print(f'Gauti taškai: {b}')
+                spare_list.append(b)
+                result[str(i)] = spare_list
                 break
 
-    print(result)
+    return result
 
 
+def calculate_frame_score() -> dict:
 
-    
-    
+    nmb_of_thrw = number_of_throws()
+   
+    frame_score = {}
+
+    for key, value in nmb_of_thrw.items():
+
+        if len(value) == 2:  # SPARE
+            if sum(value) == 10:
+                spare_value = []
+                for i in value:
+                    logging.info(f"SPARE area, i value ! {i}")
+                    spare_value.append(i)
+                int_key = int(key) + 1
+                a = nmb_of_thrw.get(str(int_key))
+                logging.info(f"SPARE area, a value ! {a}")
+                spare_value.append(a[0])
+                frame_score[key] = sum(spare_value)
+
+            else:
+                frame_score[key] = sum(value)
+
+        elif len(value) == 3:
+            frame_score[key] = sum(value)
+
+        strike_value = []
+
+        if value[0] == 10:  # STRIKE
+            for i in range(0, 3):
+                if len(strike_value) == 3:
+                    break
+                int_x = int(key) + i
+                a = nmb_of_thrw.get(str(int_x))
+
+                if a == 10:
+                    strike_value.append(a[0])
+                else:
+                    h = nmb_of_thrw.get(str(int_x))
+                    for r in h:
+                        if len(strike_value) == 3:
+                            break
+                        strike_value.append(r)
+                        
+            frame_score[key] = sum(strike_value)
+    print(frame_score)
+    return frame_score
+
+
 if __name__ == "__main__":
 
-    number_of_throws()
+    calculate_frame_score()
+    #number_of_throws()
+
--- bowling/main.py
+++ bowling/main.py
@@ -10,42 +10,10 @@ class Player:
 
 class Result:
 
-    def __init__(self, **kwargs) -> None: # TODO: reikia sutvarkyti čia kažkaip paduoti į funkciją
-        
-        frame_score = {}
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+
     
-        for x, y in kwargs.items():
-            
-            if len(y) == 2: # SPARE
-                if sum(y) == 10:
-                    spare_value = []
-                    for i in y:
-                        spare_value.append(i)
-                    a = kwargs.get(x + 1)
 
-                    spare_value.append(a[0])
-                    frame_score[x] = sum(spare_value)
-                    
-                else:
-                    frame_score[x] = sum(y)
-                    
-            elif len(y) == 3:
-                frame_score[x] = sum(y)
-                
-            elif len(y) == 1: # STRIKE
-                strike_value =[]
-                
-                for i in range(3):
-                    a = kwargs.get(x + i)
-                    for s in a:
-                        strike_value.append(s)
-                        
-                print(x, strike_value)
-                frame_score[x] = sum(strike_value[:3])
-        
-        print(frame_score)
-
-
-
-
-print(number_of_throws())
+a = Result.calculate_frame_score(**number_of_throws())
+print(a)
